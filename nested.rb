ff1c37ba7c8fe59d1ef6b311dd9b6abe0a34c770
Bria White <bria.awhite@gmail.com>
2020-08-06 23:07:24 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -14,8 +14,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+      
      }
-
+ programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    lang = programmer_hash[:dennis_ritchie][:languages]
+    
+     lang[0]
 end
 
 def adding_matz
@@ -77,9 +82,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
+        :yukihiro_matsumoto => {}
      }
-
+ 
     
 end
 
@@ -101,8 +107,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
      
-     
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -121,5 +128,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+ programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+ 
+ programmer_hash
 end
